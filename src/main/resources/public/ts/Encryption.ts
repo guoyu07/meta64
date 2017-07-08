51153bcc1a200dc492a45d8e200dba02e9b10346
clay <clay@CLAYSXPS>
2017-07-08 12:58:04 -0500

    WebCryptoAPI, export+import keys test ok

--- src/main/resources/public/ts/Encryption.ts
+++ src/main/resources/public/ts/Encryption.ts
@@ -1,5 +1,6 @@
 console.log("Encryption.ts");
 
+import { util } from "./Util";
 import { EncryptionKeyPair } from "./EncryptionKeyPair";
 
 /* This class is for proof-of-concept work related to doing Public Key Encryption on the client using the
@@ -8,12 +9,19 @@ a call to 'new Encryption().test()' from an admin option
 */
 export class Encryption {
 
+    static KEY_SAVE_FORMAT = "jwk";
     static ENC_ALGO = "RSA-OAEP";
     static HASH_ALGO = "SHA-256";
+    static ALGO_OPERATIONS = ["encrypt", "decrypt"];
+    static OP_ENCRYPT: string[] = ["encrypt"];
+    static OP_DECRYPT: string[] = ["decrypt"];
 
     crypto = window.crypto || (<any>window).msCrypto;
     vector = null;
     keyPair = new EncryptionKeyPair(null, null);
+
+    publicKeyJson: string = null;
+    privateKeyJson: string = null;
 
     data = "space aliens have landed. we are not alone.";
     encrypted_data = null;
@@ -33,18 +41,98 @@ export class Encryption {
             modulusLength: 2048, //
             publicExponent: new Uint8Array([0x01, 0x00, 0x01]), //
             hash: { name: Encryption.HASH_ALGO }
-        }, false, ["encrypt", "decrypt"]);
+        }, true, Encryption.ALGO_OPERATIONS);
 
         promise.then(
             (key) => {
                 this.keyPair.privateKey = key.privateKey;
                 this.keyPair.publicKey = key.publicKey;
+
+                this.exportKeys(this.keyPair);
                 this.encrypt();
             });
 
-        promise.catch = function(e) {
+        promise.catch = (e) => {
             console.log(e.message);
         }
+    }
+
+    /*
+     Verifies that both keys can be exported to text (JSON), and then successfully reimported back from that text to usable form
+     */
+    exportKeys = (keyPair: EncryptionKeyPair) => {
+        this.crypto.subtle.exportKey(
+            Encryption.KEY_SAVE_FORMAT,
+            keyPair.publicKey
+        )
+            .then((keydata) => {
+                this.publicKeyJson = util.toJson(keydata);
+                console.log("PublicKey: " + this.publicKeyJson);
+                this.importKey(Encryption.OP_ENCRYPT, "public", this.publicKeyJson);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        this.crypto.subtle.exportKey(
+            Encryption.KEY_SAVE_FORMAT,
+            keyPair.privateKey
+        )
+            .then((keydata) => {
+                this.privateKeyJson = util.toJson(keydata);
+                console.log("PrivateKey: " + this.privateKeyJson);
+                this.importKey(Encryption.OP_DECRYPT, "private", this.privateKeyJson);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    importKey = (algoOp: string[], keyName: string, key: string) => {
+        this.crypto.subtle.importKey(
+            Encryption.KEY_SAVE_FORMAT,
+            JSON.parse(key),
+            {
+                name: Encryption.ENC_ALGO,
+                hash: { name: Encryption.HASH_ALGO },
+            },
+            true, algoOp
+        )
+            .then((publicKey) => {
+                console.log(keyName + " key successfully imported.");
+            })
+            .catch((err) => {
+                console.error("Error importing " + keyName + " key: " + err);
+            });
+    }
+
+    encrypt = () => {
+        let promise = this.crypto.subtle.encrypt({ name: Encryption.ENC_ALGO, iv: this.vector }, this.keyPair.publicKey, this.convertStringToArrayBufferView(this.data));
+
+        promise.then(
+            (result) => {
+                this.encrypted_data = new Uint8Array(result);
+                console.log("Encrpted data: " + this.convertArrayBufferViewtoString(this.encrypted_data));
+                this.decrypt();
+            },
+            (e) => {
+                console.log(e.message);
+            }
+        );
+    }
+
+    decrypt = () => {
+        let promise = this.crypto.subtle.decrypt({ name: Encryption.ENC_ALGO, iv: this.vector }, this.keyPair.privateKey, this.encrypted_data);
+
+        promise.then(
+            (result) => {
+                this.decrypted_data = new Uint8Array(result);
+                console.log("Decrypted data: " + this.convertArrayBufferViewtoString(this.decrypted_data));
+            },
+            (e) => {
+                console.log(e.message);
+            }
+        );
     }
 
     convertStringToArrayBufferView = (str) => {
@@ -61,34 +149,5 @@ export class Encryption {
             str += String.fromCharCode(buffer[i]);
         }
         return str;
-    }
-
-    encrypt = () => {
-        let promise = this.crypto.subtle.encrypt({ name: Encryption.ENC_ALGO, iv: this.vector }, this.keyPair.publicKey, this.convertStringToArrayBufferView(this.data));
-
-        promise.then(
-            (result) => {
-                this.encrypted_data = new Uint8Array(result);
-                console.log("Encrpted data: " + this.convertArrayBufferViewtoString(this.encrypted_data));
-                this.decrypt();
-            },
-            function(e) {
-                console.log(e.message);
-            }
-        );
-    }
-
-    decrypt = () => {
-        let promise = this.crypto.subtle.decrypt({ name: Encryption.ENC_ALGO, iv: this.vector }, this.keyPair.privateKey, this.encrypted_data);
-
-        promise.then(
-            (result) => {
-                this.decrypted_data = new Uint8Array(result);
-                console.log("Decrypted data: " + this.convertArrayBufferViewtoString(this.decrypted_data));
-            },
-            function(e) {
-                console.log(e.message);
-            }
-        );
     }
 }

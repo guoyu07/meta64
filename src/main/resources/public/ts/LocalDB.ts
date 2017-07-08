a6b9dd3c30dfc0f632a04cf0d25aa2662afbcb9f
clay <clay@CLAYSXPS>
2017-07-08 17:52:03 -0500

    LocalDB.ts initial cleanup.

--- src/main/resources/public/ts/LocalDB.ts
+++ src/main/resources/public/ts/LocalDB.ts
@@ -6,6 +6,8 @@ Instantiate a new instance of this object for each operation you perform.
 */
 export class LocalDB {
 
+    static STORE_NAME = "SubNodeObjStore";
+    static DB_NAME = "SubNodeDB";
     static VERSION = 1;
 
     //todo-0: provide proper error handling for when indexedDB cannot be initialized.
@@ -14,43 +16,23 @@ export class LocalDB {
 
     /* Saves an object under the specified name. Basically emulating a simple "map" with string key. */
     writeObject = (val: Object): any => {
-        // Open (or create) the database
-        let open = indexedDB.open("MyDatabase", LocalDB.VERSION);
+        if (!this.indexedDB) {
+            throw "IndexedDB API not available in browser.";
+        }
+        let open = indexedDB.open(LocalDB.DB_NAME, LocalDB.VERSION);
 
-        // Create the schema
         open.onupgradeneeded = () => {
-            debugger;
             let db = open.result;
-            let store = db.createObjectStore("MyObjectStore", { keyPath: "name" });
-            //let index = store.createIndex("NameIndex", ["name.last", "name.first"]);
-            //this.index = this.store.createIndex("NameIndex", ["name"]);
+            let store = db.createObjectStore(LocalDB.STORE_NAME, { keyPath: "name" });
         };
 
         open.onsuccess = () => {
-            debugger;
-            // Start a new transaction
             let db = open.result;
-            let tx = db.transaction("MyObjectStore", "readwrite");
-            let store = tx.objectStore("MyObjectStore");
-            //this.index = this.index || this.store.index("NameIndex");
+            let tx = db.transaction(LocalDB.STORE_NAME, "readwrite");
+            let store = tx.objectStore(LocalDB.STORE_NAME);
 
-            // // Add some data
-            //store.put({ id: 12345, name: { first: "John", last: "Doe" }, age: 42 });
-            store.put({ name : name, val : val});
-            //
-            // // Query the data
-            // var getJohn = store.get(12345);
-            // var getBob = index.get(["Smith", "Bob"]);
-            //
-            // getJohn.onsuccess = function() {
-            //     console.log(getJohn.result.name.first);  // => "John"
-            // };
-            //
-            // getBob.onsuccess = function() {
-            //     console.log(getBob.result.name.first);   // => "Bob"
-            // };
+            store.put(val);
 
-            // Close the db when the transaction is done
             tx.oncomplete = () => {
                 db.close();
             };
@@ -59,43 +41,26 @@ export class LocalDB {
 
     /* Reads an object by name and asynchronously sends it to the callback as the 'val' parameter */
     readObject = (name: string, dataSuccessCallback: (val: Object) => void): any => {
-        // Open (or create) the database
-        let open = indexedDB.open("MyDatabase", LocalDB.VERSION);
+        if (!this.indexedDB) {
+            throw "IndexedDB API not available in browser.";
+        }
+        let open = indexedDB.open(LocalDB.DB_NAME, LocalDB.VERSION);
 
-        // Create the schema
         open.onupgradeneeded = () => {
-            debugger;
             let db = open.result;
-            let store = db.createObjectStore("MyObjectStore", { keyPath: "name" });
-            //let index = store.createIndex("NameIndex", ["name.last", "name.first"]);
-            //this.index = this.store.createIndex("NameIndex", ["name"]);
+            let store = db.createObjectStore(LocalDB.STORE_NAME, { keyPath: "name" });
         };
 
         open.onsuccess = () => {
-            debugger;
-            // Start a new transaction
             let db = open.result;
-            let tx = db.transaction("MyObjectStore", "readwrite");
-            let store = tx.objectStore("MyObjectStore");
-            //this.index = this.index || this.store.index("NameIndex");
+            let tx = db.transaction(LocalDB.STORE_NAME, "readonly");
+            let store = tx.objectStore(LocalDB.STORE_NAME);
 
-            // // Add some data
-            //store.put({ id: 12345, name: { first: "John", last: "Doe" }, age: 42 });
-            //this.store.put({ name : name, val : val});
-            //
-            // // Query the data
             let promise = store.get(name);
-            // var getBob = index.get(["Smith", "Bob"]);
-            //
             promise.onsuccess = () => {
                 dataSuccessCallback(promise.result);
             };
-            //
-            // getBob.onsuccess = function() {
-            //     console.log(getBob.result.name.first);   // => "Bob"
-            // };
 
-            // Close the db when the transaction is done
             tx.oncomplete = () => {
                 db.close();
             };
